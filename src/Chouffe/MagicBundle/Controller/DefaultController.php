5e1c181d4abb551d80ebdfc7c9553542697db949
Arthur Caillau <caillau@kth.se>
2012-12-15 16:40:44 +0100

    Add CoreSphereConsole

--- src/Chouffe/MagicBundle/Controller/DefaultController.php
+++ src/Chouffe/MagicBundle/Controller/DefaultController.php
@@ -3,11 +3,15 @@
 namespace Chouffe\MagicBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Chouffe\MagicBundle\Entity\News;
+use Chouffe\MagicBundle\Form\NewsType;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('ChouffeMagicBundle:Default:index.html.twig', array('name' => $name));
+        $news = new News();
+        $form = $this->createForm(new NewsType(), $news);
+        return $this->render('ChouffeMagicBundle:Default:index.html.twig', array('name' => $name, 'form' => $form->createView()));
     }
 }
---
+++ src/Chouffe/MagicBundle/Form/NewsType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Chouffe\MagicBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class NewsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Chouffe\MagicBundle\Entity\News'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'chouffe_magicbundle_newstype';
+    }
+}
